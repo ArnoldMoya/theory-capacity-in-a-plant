cc074b84f89409c1305e2ca8261ac1a2722df76e
Arnold Moya <63505927+ArnoldMoya@users.noreply.github.com>
2021-07-20 10:53:11 -0500

    Update analisis_planta_excepciones.R

--- analisis_planta_excepciones.R
+++ analisis_planta_excepciones.R
@@ -2,13 +2,13 @@ library(readxl)
 library(writexl)
 library(dplyr)
 # PRODUCCION
-datos.produccion = read_excel("D:/COMACSA/datos_produccion.xlsx")
+datos.produccion = read_excel("D:/datos_produccion.xlsx")
 produccion = data.frame(datos.produccion)
 # Seleccion de las filas importantes
-produccion <- produccion %>% select(AÒo,UNIDAD,Producto,NOM_PROD,FECHA,COD,HORAS,
+produccion <- produccion %>% select(A√±o,UNIDAD,Producto,NOM_PROD,FECHA,COD,HORAS,
               TON_HORA,N_COMP, Nom..Almacen) %>%
               filter(TON_HORA>0,HORAS>0) %>% 
-              filter(!grepl("NO CONFORME",Nom..Almacen,fixed = TRUE))# Filtrado de datos v·lidos
+              filter(!grepl("NO CONFORME",Nom..Almacen,fixed = TRUE))# Filtrado de datos v√°lidos
 # summary(produccion)
 
 # PRODUCTOS
@@ -27,12 +27,12 @@ for (i in 1:nrow(productos)){
     filtro <-  productos %>% filter(S==productos$S[i])
     #head(filtro)
     tabla_filtrada <- produccion %>% filter(UNIDAD==productos$MOLINO[i]) %>%
-      filter(COD %in% filtro$COD)# %>% filter(AÒo != 2020)
+      filter(COD %in% filtro$COD)# %>% filter(A√±o != 2020)
     #head(tabla_filtrada)
     capacidades[i] <- quantile(tabla_filtrada$TON_HORA,0.75)
   }
 }
 
 # Escribe un archivo excel con las capacidades
-write_xlsx(data.frame(capacidades),path = "D:/COMACSA/capacidades_especial_con2020.xlsx",
+write_xlsx(data.frame(capacidades),path = "D:/capacidades_especial_con2020.xlsx",
            col_names = TRUE)
